74361a8b7ac698940603fc0c61f7148185d3829d
Érick da Silva Lima <eierickdasilva@gmail.com>
2025-02-14 19:18:35 -0300

    feat: adding function of adding paths to folders for backup

--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 #include <string>
+#include <filesystem>
+#include <map>
+#include <functional>
+#include <fstream>
 
 #ifdef _WIN32
     #include <windows.h>
@@ -7,6 +11,7 @@
 #else
     #include <unistd.h>
     #include <pwd.h>
+    #include <cstdlib>
 #endif
 
 using namespace std;
@@ -29,15 +34,71 @@ string getUser() {
     return "Unknown";
 }
 
+string helpMsg() {
+    return "add - add current path for backup\n";
+}
+
+void addPath() {
+    string home = getenv("HOME");
+    filesystem::path dataBackup = home + "/.local/share/byterush/path";
+
+    string path = filesystem::current_path();
+
+    if (filesystem::exists(dataBackup)) {
+        ofstream file(dataBackup, ios::app);
+
+        if (file) {
+            file << path << endl;
+            cout << "path " << path << " has been added successfully" << endl;
+        } else {
+            cout << "Error in add " << path << endl;
+        }
+    } else {
+        filesystem::path dirParent = dataBackup.parent_path();
+        if (!filesystem::exists(dirParent)) {
+            if (filesystem::create_directories(dirParent)) {
+                cout << "Created the backup data directory!\n";
+            } else {
+                cerr << "Failed to create directory :/\n";
+            }
+        }
+        
+        ofstream fileBackup(dataBackup, ios::app);
+        if (fileBackup) {
+            cout << "Backups data file created!\n";
+          
+            if (fileBackup.is_open()) {
+                fileBackup << path << "\n";
+                cout << "path " << path << " has been added successfully" << endl;
+            } else {
+                cout << "Error in add " << path << endl;
+            }
+          
+            fileBackup.close();
+        } else {
+            cerr << "Failed to create backup data file :/\n";
+        }
+    }
+}
+
 string opening() {
-    return getUser() + ", hello in Byterush!";
+    return getUser() + ", hello in Byterush!\n";
 }
 
 int main(int argc, char* argv[]) {
-    string command = arcv[1];
+    map<string, function<void()>> commands = {
+        {"add", addPath},
+    };
 
-    cout << opening() << endl;
+    string command = argv[1];
 
-    cout << argv[1] << endl;
+    // cout << opening() << endl;
+
+    if (commands.count(command)) {
+        commands[command]();
+    } else {
+        cout << helpMsg() << endl;
+    }
+
     return 0;
 }
